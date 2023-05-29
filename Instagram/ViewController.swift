a0be7fd695550887197f1b7a4a3ff5c37c98c393
Talha Batuhan Irmalı <batuqwert1@gmail.com>
2023-05-29 18:05:30 +0300

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Talha Batuhan Irmalı on 29.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
